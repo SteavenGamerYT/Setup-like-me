40d34c00bacf9e8110006b5e4befc24dc30e7b95
SteavenGamerYT <62666559+SteavenGamerYT@users.noreply.github.com>
2021-12-02 22:37:18 +0200

    Update setup.cmd

--- Do4ntomize/setup.cmd
+++ Do4ntomize/setup.cmd
@@ -1,4 +1,6 @@
 echo off
+cd %systemroot%\system32
+call :IsAdmin
 echo Installing Echo so apps can be installed
 powershell "Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))"
 cls
@@ -11,3 +13,16 @@ powershell "Invoke-WebRequest https://github.com/SteavenGamerYT/Setup-like-me/ra
 reg add "HKEY_CURRENT_USER\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d c:\wallpaper.jpg /f
 RUNDLL32.EXE USER32.DLL,UpdatePerUserSystemParameters ,1 ,True
 echo wallpaper changed
+echo Adding This PC shortcut ete
+Reg.exe add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\NewStartPanel" /v "{59031a47-3f72-44a7-89c5-5595fe6b30ee}" /t REG_DWORD /d "0" /f
+Reg.exe add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\NewStartPanel" /v "{20D04FE0-3AEA-1069-A2D8-08002B30309D}" /t REG_DWORD /d "0" /f
+Reg.exe add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\NewStartPanel" /v "{F02C1A0D-BE21-4350-88B0-7367FC96EF3C}" /t REG_DWORD /d "0" /f
+Reg.exe add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\NewStartPanel" /v "{5399E694-6CE5-4D6C-8FCE-1D8870FDCBA0}" /t REG_DWORD /d "0" /f
+:IsAdmin
+Reg.exe query "HKU\S-1-5-19\Environment"
+If Not %ERRORLEVEL% EQU 0 (
+ Cls & Echo You must have administrator rights to continue ... 
+ Pause & Exit
+)
+Cls
+goto:eof
