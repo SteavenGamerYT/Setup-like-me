6cd668a098d60bbb43e5bd0531c958bcf74c1a2c
SteavenGamerYT <62666559+SteavenGamerYT@users.noreply.github.com>
2021-12-02 22:24:40 +0200

    Update setup.cmd

--- Do4ntomize/setup.cmd
+++ Do4ntomize/setup.cmd
@@ -7,5 +7,5 @@ echo Installing apps
 choco install brave winrar spotify steam-client epicgameslauncher 7zip anydesk.install git github.desktop vscode -y
 echo if apps didnt install re do the command
 echo chaning wallpaper
-powershell "Invoke-WebRequest https://raw.githubusercontent.com/SteavenGamerYT/Setup-like-me/Do4ntomize/main/wallpaper.png -OutFile C:\wallpaper.jpg"
+powershell "Invoke-WebRequest https://github.com/SteavenGamerYT/Setup-like-me/raw/main/Do4ntomize/wallpaper.jpg -OutFile C:\wallpaper.jpg"
 powershell "Set-WallPaper -Image "c:\wallpaper.jpg""
